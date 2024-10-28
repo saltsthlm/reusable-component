f49dfd98afde901da603ce224295c74d91b80032
CarlUdo <carl@wbsweden.com>
2024-10-28 14:25:57 +0100

    Add onClick event to Button component

--- src/components/button.tsx
+++ src/components/button.tsx
@@ -3,11 +3,12 @@ import styles from "./button.module.css";
 type ButtonProps = {
   children: React.ReactNode;
   customStyles?: React.CSSProperties;
+  onClick?: (e: React.MouseEvent<HTMLButtonElement, MouseEvent>) => void;
 };
 
-export function Button({ children, customStyles }: ButtonProps) {
+export function Button({ children, customStyles, onClick }: ButtonProps) {
   return (
-    <button style={customStyles} className={styles.genericButton}>
+    <button style={customStyles} className={styles.button} onClick={onClick}>
       {children}
     </button>
   );
