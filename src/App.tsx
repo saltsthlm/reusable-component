0b643a8698072306424199af702dc70237edf565
CarlUdo <carl@wbsweden.com>
2024-10-28 11:38:55 +0100

    Make an App component and include different Button components

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,34 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import "./App.css";
+import { Button } from "./components/button";
+import { Header } from "./components/header";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const firstCustomStyles = {
+    color: "#650060",
+    backgroundColor: "hotpink",
+  };
+
+  const secondCustomStyles = {
+    color: "yellow",
+    backgroundColor: "red",
+  };
 
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <Header />
+      <div className="button-board">
+        <Button>
+          <span>Button 1</span>
+        </Button>
+        <Button customStyles={firstCustomStyles}>
+          <span>Button 2</span>
+        </Button>
+        <Button customStyles={secondCustomStyles}>
+          <span>Button 3</span>
+        </Button>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
